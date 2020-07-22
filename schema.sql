c374897f6f66e1d96d6631259bf1864a836f9bc1
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-07-22 03:44:44 -0700

    Created sql database tables for department and role information

--- schema.sql
+++ schema.sql
@@ -1,3 +1,25 @@
 DROP DATABASE IF EXISTS workplace_db;
 CREATE DATABASE workplace_db;
 USE DATABASE workplace_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT
+    -- holds department name--
+    name VARCHAR(30)
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL,
+    title  VARCHAR(30),
+    salary DECIMAL(6,2),
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    
+
+    PRIMARY KEY (id)
+);
